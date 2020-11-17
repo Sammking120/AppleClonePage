57d662726e83dc1117aa582c7c6cd7fb14222855
sammking <sammking120@gmail.com>
2020-11-17 18:03:12 +0300

    The final Project of Apple Clone Page

---
+++ main.js
@@ -0,0 +1,9 @@
+function focusIn(){
+	document.getElementById("search").style.width = "130px";
+	document.getElementById("search").style.transition = "2s";
+}
+
+function focusOut(){
+	document.getElementById("search").style.width = "95px";
+	document.getElementById("search").style.transition = "2s";
+}
